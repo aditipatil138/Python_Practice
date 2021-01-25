a5da188bc348f36df63ad697915760159f1676aa
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2021-01-25 15:09:02 -0800

    Add files via upload

---
+++ python_version.py
@@ -0,0 +1,4 @@
+import sys
+print("Python Version: "+ sys.version)
+print("Version Info")
+print(sys.version_info)
---
+++ string_placement.py
@@ -0,0 +1,1 @@
+print("Twinkle Twinkle little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky,\nTwinkle Twin,le little star,\n\tHow I wonder what you are") 
