50eb961f86343b1eb1c15fd2ba5e8a221191ca65
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2021-01-26 15:56:57 -0800

    Add files via upload

---
+++ formula_computation.py
@@ -0,0 +1,6 @@
+#computes the value of n+nn+nnn.  5+55+555=615
+n = int(input("Enter an Integer Number: "))
+n2=n*11
+n3=n*111
+sum=n+n2+n3
+print(sum)
---
+++ print_calender.py
@@ -0,0 +1,4 @@
+import calendar
+y = int(input("Enter the year  want: "))
+m = int(input("Enter the month you want: "))
+print(calendar.month(y,m))
