c284c3c17111559caa1ed8f86a3c89cd4f26124e
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2021-02-05 12:40:48 -0800

    Add files via upload

---
+++ distance_bet_points.py
@@ -0,0 +1,5 @@
+import math
+a = [6,9]
+b= [9,12]
+distance = math.sqrt(((b[0]-a[0])**2)+((b[1]-a[1])**2))
+print("Distance between a and b = ", distance)
---
+++ problem_math.py
@@ -0,0 +1,4 @@
+x=4
+y=3
+sum = (x + y) * (x + y)
+print("({}+{})^2={}".format(x,y,sum))
